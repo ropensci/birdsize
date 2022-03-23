5535f1954f2f423cc228c17dab9c0e9bb7dc71e6
Renata Diaz <diaz.renatam@gmail.com>
2022-03-23 17:24:25 -0400

    put .github in rbuildignore and start writing tests for included data fxns

---
+++ tests/testthat/test_02_included_data.R
@@ -0,0 +1,31 @@
+test_that("raw_masses has expected properties", {
+
+  raw_masses <- raw_masses
+
+  expect_true(nrow(raw_masses) == 897)
+  expect_true(ncol(raw_masses) == 18)
+
+  expect_true(sum(is.na(raw_masses$sd)) == 327)
+
+  expect_true(sum(is.na(raw_masses$mass)) == 6)
+
+}
+)
+
+test_that("resolving taxonomic changes works as expected", {
+
+
+  cleaned_dat <- birdsize:::clean_sp_size_data(raw_masses)
+
+  expect_true(nrow(cleaned_dat) == 913)
+  expect_true(ncol(cleaned_dat) == 16)
+
+  raw_no_changes <- dplyr::filter(raw_masses, is.na(not_in_dunning)) %>%
+    dplyr::select(-english_common_name, -sporder, -family)
+  cleaned_no_changes <- dplyr::filter(cleaned_dat, is.na(added_flag)) %>%
+    dplyr::select(-added_flag)
+
+  expect_identical(raw_no_changes, cleaned_no_changes)
+
+}
+)
