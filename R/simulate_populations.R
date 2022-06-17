f3d8eaf3d38af239163f66a61a7571686c0f0796
Renata Diaz <diaz.renatam@gmail.com>
2022-06-17 16:59:24 -0400

    refactor to use Species class, restructures such that looking up species parameters happens distinct from drawing populations. Seems cleaner to me.

--- R/simulate_populations.R
+++ R/simulate_populations.R
@@ -40,51 +40,6 @@ ind_draw <- function(species_mean = NULL, species_sd = NULL, species_abundance =
   population
 }
 
-#' Look up species mean and sd body size given species ID
-#'
-#' @param species_code species_ID as specified in the Breeding Bird Survey
-#' @param genus genus
-#' @param species species
-#'
-#' @return data frame with columns species_id, genus, species, mean_mass, mean_sd, contains_estimates
-#' @export
-#'
-#' @importFrom dplyr filter
-#'
-species_lookup <- function(species_code = NULL, genus = NULL, species = NULL) {
-  sd_table <- sd_table
-
-  if (!is.null(species_code)) {
-    if (!(species_code %in% sd_table$species_id)) {
-      stop("`species_code` is invalid.")
-    }
-
-    return(sd_table %>%
-             dplyr::filter(.data$species_id == species_code))
-  } else if (all(is.character(genus), is.character(species))) {
-    proper_genus <- tolower(genus)
-    substr(proper_genus, 1, 1) <- toupper(substr(proper_genus, 1, 1))
-    proper_species <- tolower(species)
-
-    sp_pars <- dplyr::filter(
-      sd_table,
-      genus == proper_genus,
-      species == proper_species
-    )
-
-    valid_name <- nrow(sp_pars) == 1
-
-    if (valid_name) {
-      return(sp_pars)
-    } else {
-      stop("`genus` `species` combination is invalid.")
-    }
-  } else {
-    stop("Either `species_code` or both `genus` and `species` must be provided.")
-  }
-}
-
-
 
 
 #' Simulate body masses for a population
@@ -93,66 +48,38 @@ species_lookup <- function(species_code = NULL, genus = NULL, species = NULL) {
 #'
 #' Fills in the necessary parameters based on the parameters provided and passes these to [ind_draw()].
 #'
-#' `species_abundance`, and *either* `species_mean` or `species_code`, must be provided. Depending on which parameters are provided:
+#' @param abundance integer number of individuals to draw. *Required*.
+#' @param aou aou
+#' @param genus genus
+#' @param species species
+#' @param mean_size numeric, mean body mass (in grams) for this species.
+#' @param sd_size numeric, standard deviation of body mass for this species.
+#' @param id defaults AOU or 1
 #'
-#' * `species_mean` and `species_sd`: If both mean and standard deviation body size are provided, uses the values provided to draw `species_abundance` individuals.
-#' * `species_mean` but no `species_sd`: If mean is provided but standard deviation is not, estimates the standard deviation based on the scaling relationship between mean and standard deviation of body mass (see [species_estimate_sd()]).
-#' * `species_code` or both `genus` and `species` but no `species_mean` or `species_sd`: If parameter values are not provided, but a species code is, look up the mean and standard deviation body mass for that species (see [species_lookup()].
-#' * If both species ID (`species_code`) _and_ `species_mean` are provided, uses the species ID and ignores the mean provided.
-#'
-#' @param species_abundance integer number of individuals to draw. *Required*.
-#' @param species_mean numeric, mean body mass (in grams) for this species.
-#' @param species_sd numeric, standard deviation of body mass for this species.
-#' @param species_code species ID for this species.
-#'
-#' @return a dataframe with `species_abundance` rows and columns for: `species_code`, `genus`, `species`, `species_mean`, `species_sd`, `species_abundance`, `simulation_method`, and `individual_mass`.
+#' @return a dataframe with `abundance` rows and columns for species attributes.
 #' @export
-pop_generate <- function(species_abundance = NULL, species_mean = NULL, species_sd = NULL, species_code = NULL) {
-  if (all(!is.null(species_code), !is.null(species_mean))) {
-    message("Both `species_code` and `species_mean` are provided; using `species_code` and overwriting `species_mean` based on species' parameters.")
-  }
+pop_generate <- function(abundance = NULL, aou = NULL, genus = NULL, species = NULL, mean_size = NULL, sd_size = NULL, id = 1) {
+
+  this_species <- species_define(aou = aou,
+                                genus = genus,
+                                species = species,
+                                mean_size = mean_size,
+                                sd_size = sd_size,
+                                id = id)
 
 
-  if (!is.null(species_code)) {
-    species_pars <- species_lookup(species_code)
-
-    species_mean <- species_pars$mean_mass
-    species_sd <- species_pars$mean_sd
-
-
-    population <- ind_draw(species_mean = species_mean, species_sd = species_sd, species_abundance = species_abundance)
-
-    simulation_method <- "species code provided"
-  } else if (!is.null(species_mean)) {
-    species_code <- NA
-
-    simulation_method <- "mean and sd provided"
-
-    if (!is.numeric(species_mean)) {
-      stop("`species_mean`, if used, must be numeric")
-    }
-
-    if (is.null(species_sd)) {
-      species_sd <- species_estimate_sd(species_mean)
-      simulation_method <- "sd estimated from mean provided"
-    }
-
-    population <- ind_draw(
-      species_abundance = species_abundance,
-      species_mean = species_mean,
-      species_sd = species_sd
-    )
-  } else {
-    stop("Either `species_mean` or `species_code` must be provided.")
-  }
+    this_population <- ind_draw(species_mean = this_species@mean_size, species_sd = this_species@sd_size, species_abundance = abundance)
 
   population_df <- data.frame(
-    species_code = species_code,
-    species_mean_mass = species_mean,
-    species_sd_mass = species_sd,
-    species_abundance = species_abundance,
-    simulation_method = simulation_method,
-    individual_mass = population
+    aou = this_species@aou,
+    id = this_species@id,
+    genus = this_species@genus,
+    species = this_species@species,
+    individual_mass = this_population,
+    mean_size = this_species@mean_size,
+    sd_size = this_species@sd_size,
+    abundance = abundance,
+    sd_method = this_species@sd_method
   )
 
   population_df
---
+++ R/species_define.R
@@ -0,0 +1,122 @@
+#' Define a species
+#'
+#' Populates a [Species] object with taxonomic/identifying information and parameters for mean and standard deviation of body mass. Order of preference: AOU > genus + species > user provided mean and sd > user provided mean and estimated sd.
+#'
+#' @param aou AOU
+#' @param genus genus
+#' @param species species
+#' @param mean_size mean body size
+#' @param sd_size sd of body size
+#' @param id identifier; if using taxonomic info, defaults to AOU. If not, defaults to 1. Supplying other values can be useful for simulation models.
+#'
+#' @return object of class Species
+#' @export
+#'
+species_define <- function(aou = NULL, genus = NULL, species = NULL, mean_size = NULL, sd_size = NULL, id = 1) {
+
+  if(!is.null(aou)) {
+
+    # use AOU to get mean, sd, genus, and species
+    spPars <- species_lookup(species_code = aou)
+    thisSpecies <- new("Species", aou = aou, genus = spPars$genus, species = spPars$species, mean_size = spPars$mean_mass, sd_size = spPars$mean_sd, sd_method = "AOU lookup", id = aou)
+
+    # Check that any user-supplied taxonomic info matches the AOU provided
+    if(!is.null(genus)) {
+
+      if(genus != thisSpecies$genus) { warning("User-provided genus does not match genus associated with this AOU") }
+
+    }
+
+    if(!is.null(species)) {
+
+      if(species != thisSpecies$species) { warning("User-provided species does not match species associated with this AOU") }
+
+    }
+
+    return(thisSpecies)
+  }
+
+  # If AOU is not provided (implicit in order) try genus + species
+
+  if(all(!is.null(genus), !is.null(species))) {
+
+    spPars <- species_lookup(genus = genus, species = species)
+    thisSpecies <- new("Species", aou = aou, genus = spPars$genus, species = spPars$species, mean_size = spPars$mean_mass, sd_size = spPars$mean_sd, sd_method = "Scientific name lookup", id = aou)
+    return(thisSpecies)
+  }
+
+  # If neither of AOU or genus + species is provided (implicit in order)
+  if(!is.null(mean_size)) {
+    if(!is.null(sd_size)) {
+      thisSpecies <- new("Species", aou = as.numeric(NA), genus = as.character(NA), species =as.character(NA),  mean_size = mean_size, sd_size = sd_size, sd_method = "Mean and SD provided", id = id)
+      return(thisSpecies)
+    }
+
+    this_sd <- species_estimate_sd(mean_size)
+    thisSpecies <- new("Species", aou = as.numeric(NA), genus = as.character(NA), species =as.character(NA), mean_size = mean_size, sd_size = this_sd, sd_method = "SD estimated from mean", id = id)
+    return(thisSpecies)
+  }
+
+  # If insufficient information is provided, throw an error
+
+  stop("At least one of: AOU, genus + species, or mean_size must be provided!")
+
+}
+
+#' An S4 class to hold species information
+#'
+#' @slot aou The AOU
+#' @slot genus genus
+#' @slot species species
+#' @slot mean_size size
+#' @slot sd_size sd of size
+#' @slot sd_method known, or estimated
+#' @slot id if AOU is provided, AOU; otherwise an integer for keeping track of hypothetical species
+
+Species <- setClass("Species", slots = list(aou = "numeric", genus = "character", species = "character", mean_size = "numeric", sd_size = "numeric", sd_method = "character", id = "numeric"))
+
+#' Look up species mean and sd body size given species ID
+#'
+#' @param species_code species_ID as specified in the Breeding Bird Survey
+#' @param genus genus
+#' @param species species
+#'
+#' @return data frame with columns species_id, genus, species, mean_mass, mean_sd, contains_estimates
+#' @export
+#'
+#' @importFrom dplyr filter
+#'
+species_lookup <- function(species_code = NULL, genus = NULL, species = NULL) {
+  sd_table <- sd_table
+
+  if (!is.null(species_code)) {
+    if (!(species_code %in% sd_table$species_id)) {
+      stop("`species_code` is invalid.")
+    }
+
+    return(sd_table %>%
+             dplyr::filter(.data$species_id == species_code))
+  } else if (all(is.character(genus), is.character(species))) {
+    proper_genus <- tolower(genus)
+    substr(proper_genus, 1, 1) <- toupper(substr(proper_genus, 1, 1))
+    proper_species <- tolower(species)
+
+    sp_pars <- dplyr::filter(
+      sd_table,
+      genus == proper_genus,
+      species == proper_species
+    )
+
+    valid_name <- nrow(sp_pars) == 1
+
+    if (valid_name) {
+      return(sp_pars)
+    } else {
+      stop("`genus` `species` combination is invalid.")
+    }
+  } else {
+    stop("Either `species_code` or both `genus` and `species` must be provided.")
+  }
+}
+
+
