4b9cf88600ac5fadb00981dbcd8988006b89ad49
Renata Diaz <diaz.renatam@gmail.com>
2022-06-24 14:18:14 -0400

    starting to work towards updating raw_masses to communicate with most recent BBS data release

---
+++ R/new_hartford_raw.R
@@ -0,0 +1,31 @@
+#' Raw data for the New Hartford Breeding Bird Survey route.
+#'
+#' Downloaded using the rdataretriever in June 2022.
+#' For inclusion in package vignettes, function testing.
+#' Citation needed!
+#'
+#' @format A data frame with 1470 rows and 17 variables:
+#' \describe{
+#'   \item{record_id}{inherited}
+#'   \item{routedataid}{inherited}
+#'   \item{countrynum}{inherited}
+#'   \item{statenum}{inherited}
+#'   \item{route}{inherited}
+#'   \item{rpid}{inherited}
+#'   \item{year}{inherited}
+#'   \item{aou}{inherited}
+#'   \item{rpid}{inherited}
+#'   \item{count10}{inherited}
+#'   \item{count20}{inherited}
+#'   \item{count30}{inherited}
+#'   \item{count40}{inherited}
+#'   \item{count50}{inherited}
+#'   \item{stoptotal}{inherited}
+#'   \item{speciestotal}{inherited}
+#' }
+#' @references \itemize{
+#'   \item{Pardieck, K. L., Ziolkowski, D. J., Lutmerding, M., Aponte, V., & Hudson, M.-A. (2019). North American Breeding Bird Survey Dataset 1966—2018, version 2018.0. U.S. Geological Survey. https://doi.org/10.5066/P9HE8XYJ}
+#' }
+#'
+#' @keywords internal
+#'
