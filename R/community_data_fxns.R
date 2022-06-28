a70085be20856feb39905e1daa1ecd6677a068f4
Renata Diaz <diaz.renatam@gmail.com>
2022-06-28 17:01:40 -0400

    something's wrong with community_data_fxns use of species and genus in purrr

--- R/community_data_fxns.R
+++ R/community_data_fxns.R
@@ -1,6 +1,6 @@
 #' Generate samples community-wide
 #'
-#' @param community_data_table dataframe containing at least one of `aou` or `mean_size` and a column for species abundances
+#' @param community_data_table dataframe containing at least one of `aou`, `genus` *and* `species`, or `mean_size` and a column for species abundances
 #' @param abundance_column_name character, the name of the column with species abundances. Defaults to "speciestotal"
 #' @return dataframe
 #' @export
@@ -20,6 +20,7 @@ community_generate <- function(community_data_table, abundance_column_name = "sp
   # Check that the necessary variables are provided
 
   contains_aou <- "aou" %in% community_vars
+  contains_genus_species <- all("genus" %in% community_vars, "species" %in% community_vars)
   contains_mean <- "mean_size" %in% community_vars
   contains_abundance <- abundance_column_name %in% community_vars
 
@@ -27,8 +28,8 @@ community_generate <- function(community_data_table, abundance_column_name = "sp
     warning("abundance column is required. If the name is not `speciestotal` specify using the `abundance_column_name` argument")
   }
 
-  if(!(contains_aou | contains_mean)) {
-    warning("At least one of `aou` or `mean_size` is required")
+  if(!(contains_aou | contains_mean | contains_genus_species)) {
+    warning("At least one of `aou`, `genus` and `species`, or `mean_size` is required")
   }
 
   # Identify ID/grouping columns and columns to pass to sim fxns.
@@ -39,28 +40,35 @@ community_generate <- function(community_data_table, abundance_column_name = "sp
 
   community_vars_mod <- colnames(community_data_table)
 
-  possible_sim_vars <- c("abundance", "aou", "mean_size", "sd_size", "sim_species_id")
+  possible_sim_vars <- c("abundance", "aou", "mean_size", "sd_size", "sim_species_id", "species", "genus")
 
   id_vars <- c(community_vars_mod[ which(!(community_vars_mod %in% possible_sim_vars))])
 
   sim_vars <- c(community_vars_mod[ which(community_vars_mod %in% possible_sim_vars)])
 
-  # For the cols to pass in, add NA columns for any of the variables that the sim fxns can use that aren't included
-  na_vars <- possible_sim_vars[ which(!(possible_sim_vars %in% community_vars_mod))]
-
-  na_table <- matrix(nrow = nrow(community_data_table), ncol = length(na_vars)) %>%
-    as.data.frame()
-  colnames(na_table) <- na_vars
+  # # For the cols to pass in, add NA columns for any of the variables that the sim fxns can use that aren't included
+  # na_vars <- possible_sim_vars[ which(!(possible_sim_vars %in% community_vars_mod))]
+  #
+  # na_table <- matrix(nrow = nrow(community_data_table), ncol = length(na_vars)) %>%
+  #   as.data.frame()
+  # colnames(na_table) <- na_vars
 
   # Split into 2 tables, one with ID cols and one for the cols to pass in.
   ids_table <- community_data_table[,id_vars]
 
-  sim_vars_table <- community_data_table[ ,sim_vars] %>%
-    cbind(na_table)
+  sim_vars_table <- community_data_table[ ,sim_vars]
+  # %>%
+  #   cbind(na_table)
 
   # Draw populations
   populations <- purrr::pmap_dfr(sim_vars_table,
                                  pop_generate,
+                                 .id = "rejoining_id") %>%
+    dplyr::mutate(rejoining_id = as.numeric(.data$rejoining_id))
+
+
+  populations <- purrr::pmap_dfr(sim_vars_table,
+                                 show_inputs,
                                  .id = "rejoining_id") %>%
     dplyr::mutate(rejoining_id = as.numeric(.data$rejoining_id))
 
@@ -70,6 +78,22 @@ community_generate <- function(community_data_table, abundance_column_name = "sp
 
 }
 
+show_inputs <- function(abundance = NA, aou = NA, genus = NA, species = NA, mean_size = NA, sd_size = NA, sim_species_id = 1) {
+
+  inputs <- data.frame(
+    abundance = abundance,
+    aou = aou,
+    genus = genus,
+    species = species,
+    mean_size = mean_size,
+    sd_size = sd_size,
+    sim_species_id = sim_species_id
+  )
+  species_define(aou = aou, genus = genus, species = species)
+
+  return(inputs)
+
+}
 
 
 #' Check for unidentified species
