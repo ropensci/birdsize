26a7a442ed6af43baf5e659071b2267e25366e2b
Renata Diaz <diaz.renatam@gmail.com>
2022-06-30 16:07:54 -0400

    state variable functions for population and community. both need edge case exploring and tests.

--- R/metabolic_rate.R
+++ R/metabolic_rate.R
@@ -3,6 +3,8 @@
 #' @param mass mass in grams
 #' @return estimated basal metabolic rate using pars from Fristoe 2015
 #' @export
+#' @examples
+#' individual_metabolic_rate(10)
 individual_metabolic_rate <- function(mass) {
 
   return(10.5 * (mass ^ .713))
